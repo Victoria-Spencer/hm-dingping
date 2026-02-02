e593a8261c454e282a04facd0b9f644abae1235c
Victoria-Spencer <1557573258@qq.com>
2026-02-02 23:40:29 +0800

    导入数据库中店铺的geo数据到redis中

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -10,6 +10,10 @@ import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.GeoOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -22,6 +26,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -35,8 +40,9 @@ class HmDianPingApplicationTests {
     @Autowired
     private RedisIdWorker redisIdWorker;
 
-
     private ExecutorService executorService = Executors.newFixedThreadPool(500);
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void testSaveShop() throws InterruptedException {
@@ -122,5 +128,35 @@ class HmDianPingApplicationTests {
         latch.await();
         long end = System.currentTimeMillis();
         System.out.println("time："+ (end - begin));
+    }
+
+    /**
+     * 导入店铺位置信息到Redis中
+     */
+    @Test
+    void loadShopData() {
+        // 1.从数据库中查询数据
+        List<Shop> list = shopService.list();
+        // 2.将店铺进行分组，将typeId一致的放到一个集合中
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3.分批写入redis中
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 3.1.获取分组的键
+            String key = SHOP_GEO_KEY + entry.getKey();
+            // 3.2.获取分组的店铺列表，封装成Redis GEO需要的GeoLocation集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> geoLocations = new ArrayList<>();
+            for (Shop shop : value) {
+                RedisGeoCommands.GeoLocation<String> location = new RedisGeoCommands.GeoLocation<String>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(),shop.getY())
+                );
+                geoLocations.add(location);
+            }
+            // 3.3.批量写入
+            if (!geoLocations.isEmpty()) {
+                stringRedisTemplate.opsForGeo().add(key, geoLocations);
+            }
+        }
     }
 }
